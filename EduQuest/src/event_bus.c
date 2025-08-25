3af9b7692d66a678113ec50f1bcc0c622101e1f4
Azeez Adebisi <samadebisi12@gmail.com>
2025-08-26 02:23:25 +0300

    Update

---
+++ EduQuest/src/event_bus.c
@@ -0,0 +1,14 @@
+// =============================================
+// file: src/event_bus.c
+// =============================================
+#include "event_bus.h"
+
+void eventbus_init(EventBus *bus){ bus->count=0; }
+
+bool eventbus_subscribe(EventBus *bus, EventHandler h, void *user){
+    if(bus->count>=MAX_SUBS) return false; bus->handlers[bus->count]=h; bus->user[bus->count]=user; bus->count++; return true;
+}
+
+void eventbus_publish(EventBus *bus, const Event *ev){
+    for(size_t i=0;i<bus->count;i++) bus->handlers[i](ev,bus->user[i]);
+}
